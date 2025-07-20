cfcfd14809aee0132667bb71302a70c968d892ef
nodegxq <nodegxq@gmail.com>
2025-07-20 15:09:11 -0700

    LC-remove-duplicates-from-sorted-array-ii [Runtime: 7 ms; Memory: 19.5 MB]

--- two-pointers/remove-duplicates-from-sorted-array-ii.cpp
+++ two-pointers/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+
+        if (nums.size() <= 2) return nums.size();  // All elements can stay if size <= 2
         
         int k = 2;   // slow pointer, first 2 elements are always allowed
 
