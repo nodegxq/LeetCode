8a7cec60397b8d4279dcc0d8cf6e020a4a89afd0
nodegxq <nodegxq@gmail.com>
2025-07-22 17:52:15 -0700

    LC-best-time-to-buy-and-sell-stock [Runtime: 0 ms; Memory: 97.3 MB]

--- dynamic-programming/best-time-to-buy-and-sell-stock.cpp
+++ dynamic-programming/best-time-to-buy-and-sell-stock.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
 
-        int maxPrice = 0;
-
-        while (!prices.empty()) {
-
-            bool remove = false;
-
-            for (int i = 1; i < prices.size(); ++i) {
-
-                if (prices[0] > prices[i]) {
-                    // First element is larger, remove it
-                    prices.erase(prices.begin());
-                    remove = true;
-                    break;
-                }
-            }
-
-            if (!remove) break; // if no element is smaller, stop
-        }
-
-        for (int j = 0; j < prices.size(); ++j){
-            if (prices.size() == 1){
-                maxPrice = prices[0];
-            } else if (prices[j] < prices[j + 1]) {
-                maxPrice = prices[j + 1];
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price; // update minPrice when a new lower price is found
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice; // update maxProfit if current profit is higher
             }
         }
 
-        return maxPrice - prices[0];
+        return maxProfit;
     }
 };
