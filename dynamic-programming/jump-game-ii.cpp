5ddabcd7a92cb73bab5a5c4c35fb5f5b7c4f387b
nodegxq <nodegxq@gmail.com>
2025-07-25 17:00:22 -0700

    LC-jump-game-ii [Runtime: 4 ms; Memory: 20.5 MB]

--- dynamic-programming/jump-game-ii.cpp
+++ dynamic-programming/jump-game-ii.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        
-        int jump = 0;
-        int left = 0;
-        int right  = 0;
+        int jumps = 0;
+        int farthest = 0;
+        int currEnd = 0;
 
-        while (left < nums.size() - 1){
-            int farthest = 0;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            farthest = max(farthest, i + nums[i]);
 
-            for (int i = left; i < nums.size() - 1; ++i){
-                farthest = max(farthest, i + nums[i]);
+            if (i == currEnd) {
+                jumps++;
+                currEnd = farthest;
             }
-
-            left = right + 1;
-            right = farthest;
-            jump += 1;
         }
 
-        return jump;
+        return jumps;
     }
 };
