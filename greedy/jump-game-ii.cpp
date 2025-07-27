29b16f772aca84090cb60727b024731fcb4ade33
nodegxq <nodegxq@gmail.com>
2025-07-27 13:23:28 -0700

    LC-jump-game-ii [Runtime: 0 ms; Memory: 20.3 MB]

--- greedy/jump-game-ii.cpp
+++ greedy/jump-game-ii.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int res = 0;
-        int l = 0, r = 0;
+        int left = 0, right = 0;
 
-        while (r < nums.size() - 1) {
+        while (right < nums.size() - 1) {
             int farthest = 0;
 
-            for (int i = l; i <= r; ++i) {
+            for (int i = left; i <= right; ++i) {
                 farthest = max(farthest, i + nums[i]);
             }
 
-            l = r + 1;
-            r = farthest;
+            left = right + 1;
+            right = farthest;
             res += 1;
         }
 
